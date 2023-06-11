dfeef58e389bef7d89331c56952a6dd861e8b6e9
Sonia Sheth <sheth.son@northeastern.edu>
2023-06-11 14:57:45 -0400

    added some touch ups

--- src/main/java/cs3500/pa04/Enums/Message.java
+++ src/main/java/cs3500/pa04/Enums/Message.java
@@ -1,16 +1,19 @@
 package cs3500.pa04.Enums;
 
 public enum Message {
-  JOIN("join"), SETUP("setup"), TAKESHOTS("take-shots"), REPORTDAMAGE("report-damage"),
-  SUCCESSFULHITS("successful-hits"), ENDGAME("end-game");
+  JOIN("join"), SETUP("setup"),
+  TAKESHOTS("take-shots"),
+  REPORTDAMAGE("report-damage"),
+  SUCCESSFULHITS("successful-hits"),
+  ENDGAME("end-game");
 
-  private final String message;
+  private final String messageName;
 
   Message(String message) {
-    this.message = message;
+    this.messageName = message;
   }
 
   public String message() {
-    return message;
+    return messageName;
   }
 }
